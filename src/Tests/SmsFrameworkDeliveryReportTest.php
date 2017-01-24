43977287d521c000e282e7f398852c55cef792d2
Aniebiet Udoh <almaudoh@yahoo.com>
2017-01-24 10:57:41 +0100

    Added automatic updates of delivery report status with test coverage.

--- src/Tests/SmsFrameworkDeliveryReportTest.php
+++ src/Tests/SmsFrameworkDeliveryReportTest.php
@@ -3,6 +3,8 @@
 namespace Drupal\sms\Tests;
 
 use Drupal\Core\Url;
+use Drupal\sms\Direction;
+use Drupal\sms\Entity\SmsDeliveryReport;
 use Drupal\sms\Message\SmsDeliveryReportInterface;
 use Drupal\sms\Message\SmsMessageReportStatus;
 use Drupal\sms\Message\SmsMessageResultInterface;
@@ -65,6 +67,57 @@ EOF;
     $this->assertEqual("status message", $second_report->getStatusMessage());
     $this->assertEqual($delivered_time, $second_report->getTimeDelivered());
     $this->assertEqual($message_id, $second_report->getMessageId());
+  }
+
+  /**
+   * Tests that delivery reports are updated after initial sending.
+   */
+  public function testDeliveryReportUpdate() {
+    $user = $this->drupalCreateUser();
+    $this->drupalLogin($user);
+
+    $test_gateway = $this->createMemoryGateway();
+    $test_gateway
+      ->setRetentionDuration(Direction::OUTGOING, 1000)
+      ->save();
+    $this->container->get('router.builder')->rebuild();
+    $sms_message = $this->randomSmsMessage($user->id())
+      ->setGateway($test_gateway)
+      ->setDirection(Direction::OUTGOING);
+
+    $this->defaultSmsProvider->queue($sms_message);
+    $this->container->get('cron')->run();
+    $saved_reports = SmsDeliveryReport::loadMultiple();
+    $this->assertEqual(count($sms_message->getRecipients()), count($saved_reports));
+    foreach ($saved_reports as $report) {
+      $this->assertEqual(SmsMessageReportStatus::QUEUED, $report->getStatus());
+    }
+
+    /** @var \Drupal\sms\Message\SmsDeliveryReportInterface $first_report */
+    $first_report = reset($saved_reports);
+    $message_id = $first_report->getMessageId();
+
+    // Get the delivery reports url and simulate push delivery report.
+    $url = $test_gateway->getPushReportUrl()->setAbsolute()->toString();
+    $delivered_time = REQUEST_TIME + rand(10, 300);
+    $delivery_report = <<<EOF
+{
+   "reports":[
+      {
+         "message_id":"{$message_id}",
+         "recipient":"{$first_report->getRecipient()}",
+         "time_sent":{$first_report->getTimeQueued()},
+         "time_delivered": $delivered_time,
+         "status_message": "status message"
+      }
+   ]
+}
+EOF;
+    $this->drupalPost($url, 'application/json', ['delivery_report' => $delivery_report]);
+    $this->container->get('entity_type.manager')->getStorage('sms_report')->resetCache();
+    $updated_report = SmsDeliveryReport::load($first_report->id());
+    $this->assertEqual(SmsMessageReportStatus::DELIVERED, $updated_report->getStatus());
+    $this->assertEqual($delivered_time, $updated_report->getTimeDelivered());
   }
 
 }
